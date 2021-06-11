f6c9cc375856fdde008ee1c4e8bd4aff0310339d
bahram-ghahari <bahram.ghahari@gmail.com>
2021-06-11 11:05:34 -0400

    added mandatory and availability checkers to validation

--- validation-engine/validate.ts
+++ validation-engine/validate.ts
@@ -5,6 +5,8 @@ import { IChecker } from "./checkers/IChecker";
 import { RegexChecker } from "./checkers/RegexChecker";
 import { TypeChecker } from "./checkers/TypeChecker";
 import { DynamicChecker } from "./checkers/DynamicChecker";
+import { MandatoryChecker } from "./checkers/MandatoryChecker";
+import { AvailabilityChecker } from "./checkers/AvailabilityChecker";
 
 /**
  * 
@@ -18,10 +20,12 @@ import { DynamicChecker } from "./checkers/DynamicChecker";
 export function validate(json_object:any , validation_params:JSONParam[]){
 
     let ret = true; 
-    let errors= new paramErrorManager(); 
+    const errors= new paramErrorManager(); 
+    const mandatory_checker = new MandatoryChecker();
+    const availability_checker = new AvailabilityChecker();
 
     //ist of checkers to check. More checkers will be added in the future
-    let checkers:IChecker[]=[
+    const checkers:IChecker[]=[
         new RegexChecker(),
         new TypeChecker(),
         new DynamicChecker()
@@ -30,14 +34,21 @@ export function validate(json_object:any , validation_params:JSONParam[]){
     validation_params.forEach(p=>{
 
         //check for mandatory fields. if not available, add  the field to error list
-        let {mandatory_and_available,available} = isMandatoryFieldAvailable(json_object,p); 
-        if(!mandatory_and_available){
-            //if not available add field name to the list of errors
-            errors.add(p.name, p.name+' is mandatory');
+
+        let field_is_mandatory = mandatory_checker.check(json_object,p);
+        let field_is_available = availability_checker.check(json_object,p); 
+
+
+        //if not available add field name to the list of errors
+        if(field_is_mandatory && !field_is_available){
+            errors.add(p.name, mandatory_checker.error(p));
             ret = false;
+            return;
         }
+
+
         //is parameter available but not mandatory? if yes, check for the rest of the conditions 
-        else if(available){
+        if(field_is_available){
 
 
             checkers.forEach(validator=>{
@@ -48,9 +59,6 @@ export function validate(json_object:any , validation_params:JSONParam[]){
                 }
             });
 
-        }
-        else{ 
-            //not much to do
         }
             
         
